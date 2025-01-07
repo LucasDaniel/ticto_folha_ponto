ee6d2a551c9fef79a79002db06bfda42b3d9777b
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-01-07 13:43:28 -0300

    Add migrate ponto

---
+++ laravel/database/migrations/0003_01_01_000000_create_work_time_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_time', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('cargo');
+            $table->string('data_nascimento');
+            $table->string('cep');
+            $table->string('endereco_completo');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_time');
+    }
+};
