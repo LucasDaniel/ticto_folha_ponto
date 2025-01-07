b37561f372c05b901f80ecea4c45e3017b40bfe5
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-01-07 18:45:18 -0300

    Adicionado Dictionary

---
+++ laravel/app/Dictionary/Dictionary.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Dictionary;
+
+trait Dictionary {
+
+    public $dictionary = [
+        'error' => [
+            'getUser' => [
+                'msg' => "Error on get user!",
+                'code' => 502
+            ]
+        ]
+    ];
+
+}
